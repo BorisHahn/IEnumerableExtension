d27b13ebfeae3288ecc42124ff5e5ecb604829fc
Boris Gaidukevich <Borishahn@yandex.ru>
2024-03-19 20:39:43 +0300

    0.0.1

---
+++ EnumerableExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace IEnumerableExtension;
+
+public static class EnumerableExtension
+{
+    public static IEnumerable<T> Top<T>(this IEnumerable<T> source, int percent)
+    {
+        if (percent < 1 || percent > 100)
+        {
+            throw new ArgumentException("Заданный параметр выходит за допустимый диапазон");
+        }
+
+        float collectionLength = source.Count();
+        int totalCount = (int)Math.Ceiling((collectionLength * percent) / 100);
+        var result = source.TakeLast(totalCount);
+        return result;
+    }
+
+    public static IEnumerable<T> Top<T>(
+        this IEnumerable<T> source, int percent, Func<T, int> selector) =>
+        source.Top(percent).OrderByDescending(selector);
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace IEnumerableExtension;
+
+class Program
+{
+    static void Main()
+    {
+        var person1 = new Person("Tom", 50);
+        var person2 = new Person("Jack", 30);
+        var person3 = new Person("Bob", 40);
+        var person4 = new Person("Michael", 43);
+        var person5 = new Person("Nick", 70);
+        var person6 = new Person("Dave", 37);
+        var person7 = new Person("Chack", 20);
+
+        var collection = new List<Person>() { person1, person2, person3, person4, person5, person6, person7 };
+        var result = collection.Top(50, el => el.Age);
+
+        foreach (var res in result)
+        {
+            Console.WriteLine(res.Age);
+        }
+    }
+}
